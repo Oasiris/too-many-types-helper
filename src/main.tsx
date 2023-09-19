d546b6cf81b81489775a84e9326cefd5903dd199
David H <davi@Artemis.local>
2023-09-18 17:56:10 -0700

    project structure and boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
+import App from './App'
+
+import './styles/lib/normalize.css'
+import './styles/lib/bootstrap-grid.min.css'
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
