00915268aabe11f59b5ba81fe143da270175e4f6
Oasiris <teracontinens@gmail.com>
2023-09-18 18:01:04 -0700

    in react router v6, Route exact is gone

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Example from './routes/Example'
+import Home from './routes/Home'
 
 const MainRoutes: React.FC = () => {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Example />} />
+            <Route path="/" element={<Home />} />
+            <Route path="example/*" element={<Example />} />
                 {/* <Route path="/" exact component={Home} /> */}
                 {/* <Route path="/" component={NotFound} /> */}
             </Routes>
