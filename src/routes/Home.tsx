6e3986562666ba5437fe47eca0991b1ab367bca3
Oasiris <teracontinens@gmail.com>
2023-09-18 17:59:01 -0700

    home

---
+++ src/routes/Home.tsx
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+        <>
+            <h1>Too Many Types Checker</h1>
+
+            <p>
+                Hello world!
+            </p>
+        </>
+    )
+}
+
+export default Home
